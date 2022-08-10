030e7c5c1429ba5105fd757e6a3f9729aede8d81
AgustinTafura <agustintafura@hotmail.com>
2022-08-10 15:44:30 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+
+import 'bootstrap/dist/css/bootstrap.css'; // Add this line
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(()=>{
+    import("bootstrap/dist/js/bootstrap");
+},[])
+
   return <Component {...pageProps} />
 }
 
