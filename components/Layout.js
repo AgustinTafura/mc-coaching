5ddc3c5507289e63432006d92b0d2256a947b65a
AgustinTafura <agustintafura@hotmail.com>
2022-08-11 15:26:00 -0300

    contactBubble

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import ContactBubble from './BubbleContact'
 const Layout = ({children}) => {
   return (
     <>
       <Navbar/>
       {children}
+      <ContactBubble/>
   </>
   )
 }
