4165c400fec416d579da14bbb449f5d3a0aa45bd
AgustinTafura <agustintafura@hotmail.com>
2022-08-22 11:55:51 -0300

    layout footer + WApp

---
+++ components/Footer.jsx
@@ -0,0 +1,114 @@
+const Footer = () => {
+
+  return (
+  <footer className="footer2 cid-rT5TRS4qUz" once="footers" id="footer2-5l">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 col-lg-3">
+          <div className="pb-4">
+            <h3 className="title pb-2 align-left mbr-bold mbr-fonts-style display-55">TherapyM4</h3>
+            <p className="mbr-text align-left  mbr-white mbr-regular mbr-fonts-style display-44">Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Vestibulum in fermentum tellus. Etiam turpis, mattis ullamcorper
+              sed, convallis eget tellus.</p>
+          </div>
+          <div className="socicon-wrap">
+            <span className="mbr-iconfont socicon-twitter socicon"></span>
+            <span className="mbr-iconfont socicon-instagram socicon"></span>
+            <span className="mbr-iconfont socicon-youtube socicon"></span>
+            <span className="mbr-iconfont socicon-instagram socicon"></span>
+          </div>
+        </div>
+        <div className="col-md-6 col-lg-3">
+          <h2 className="title mbr-bold pb-2 mbr-fonts-style display-55">
+            Frequent questions
+          </h2>
+          <div className="align-wrap">
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-right mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">
+                  How to choose a therapist?<br/></p>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-right mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">When to undergo
+                  therapy?</p>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-right mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">What do you do
+                  at therapy?</p>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-right mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">What is group
+                  counseling?</p>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-right mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">Crisis
+                  counseling?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6 col-lg-3">
+          <h2 className="title mbr-bold pb-2 mbr-fonts-style display-55">
+            Services</h2>
+          <div className="align-wrap">
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-success mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">Counseling</p>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-success mobi-mbri"></span>
+                <li className="icon-title mbr-regular mbr-fonts-style display-44">Relationships
+                </li>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-success mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">Marriage</p>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-success mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">Life coaching
+                </p>
+              </div>
+            </div>
+            <div className="item-wrap">
+              <div className="icons-wrap pb-2">
+                <span className="mbr-iconfont mobi-mbri-success mobi-mbri"></span>
+                <p className="icon-title mbr-regular mbr-fonts-style display-44">Anxiety</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6 col-lg-3">
+          <h2 className="title mbr-bold pb-2 mbr-fonts-style display-55">
+            Working hours</h2>
+          <p className="mbr-text time pb-4 mbr-white mbr-regular mbr-fonts-style display-44">Weekdays: 9:00 - 20:00
+            <br/>Saturday :&nbsp;10:00 - 18:00
+            <br/>Sunday:&nbsp;Closed</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import ContactBubble from './BubbleContact'
+import Footer from './Footer'
 const Layout = ({children}) => {
   return (
     <>
       <Navbar/>
       {children}
       <ContactBubble/>
+      <Footer/>
   </>
   )
 }
