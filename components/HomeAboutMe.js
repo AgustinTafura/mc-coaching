faac428c04866978091f3392d6b79675c5495233
AgustinTafura <agustintafura@hotmail.com>
2022-08-24 16:24:17 -0300

    aboutme updated

--- components/HomeAboutMe.js
+++ components/HomeAboutMe.js
@@ -8,73 +8,40 @@ const HomeAboutMe = () => {
                 <div className="row">
                     <div className="mbr-white col-md-12 video-col col-12 col-lg-6">
                         <div className="box">
-                            <div className="mbr-media show-modal align-center py-2" data-modal=".modalWindow">
+                            {/* <div className="mbr-media show-modal align-center py-2" data-modal=".modalWindow">
                                 <div className="icon-wrap">
                                     <span className="mbr-iconfont mobi-mbri-play mobi-mbri"></span>
                                 </div>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
 
 
                     <div className="progress_elements col-lg-6">
-                        <h4 className="mbr-section-subtitle align-left mbr-regular mbr-fonts-style display-44">ABOUT ME</h4>
+                        <h4 className="mbr-section-subtitle align-left mbr-regular mbr-fonts-style display-44">CONOCEME</h4>
 
                         <h2 className="mbr-section-title pb-3 align-left mbr-bold mbr-fonts-style mbr-white display-22">
-                            Certificates
-                            &amp; Awards</h2>
-                        <h3 className="mbr-section-text pb-4 mbr-fonts-style align-left display-77">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci delectus, beatae quae sunt
-                            maiores. Voluptatem mollitia quibusdam, veritatis. Voluptatibus ducimus non.</h3>
+                        Life Coach</h2>
+                        <p className="mbr-section-text pb-4 mbr-fonts-style align-left display-77 ">
+                            Me formé como Neurosicoeducadora y Especialista en Coaching Vocacional.
+                        </p>
+                        <p className="mbr-section-text pb-4 mbr-fonts-style align-left display-77 opacity-75 pb-md-0">
+                            Como Life Coach y en 2019 abrí mi consultorio presencial y online de Coaching.
+                        </p>
+                        <p className="mbr-section-text pb-4 mbr-fonts-style align-left display-77 opacity-75 pb-md-0 ">
+                            La pandemia y la situación de “quedarse en casa” me abrió la posibilidades de 
+                            desarrollarme ampliamente en el formato de sesiones online.
+                        </p>
+                        <p className="mbr-section-text pb-4 mbr-fonts-style align-left display-77 opacity-75 pb-md-0 ">
+                            Te invito a navegar el sitio y a conocer más sobre nuestros servicios.
+                        </p>
+                        <p className="mbr-section-text pb-4 mbr-fonts-style align-left display-77 opacity-75 pb-md-4 ">
+                            Puedes contactarme y recibiré tus inquietudes personalmente, en un marco de absoluta reserva
+                            profesional garantizada.
+                        </p>
 
+                        <a href="/conoceme" style={{color: "white",fontWeight: "500",marginTop: "1rem"}}>Más sobre mi</a>
 
-                        <div className="progress1 pb-3">
-                            <div className="title-wrap">
-                                <div className="progressbar-title mbr-fonts-style display-77">
-                                    <p>Psychology</p>
-                                </div>
-                                <div className="progress_value mbr-fonts-style display-77">
-                                    <div className="progressbar-number"></div>
-                                    <span>%</span>
-                                </div>
-                            </div>
-                            <div className="progress">
-                                <div className="progress-bar progress-bar-1" role="progressbar" aria-valuenow="100"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-
-                        <div className="progress2 pb-3">
-                            <div className="title-wrap">
-                                <div className="progressbar-title mbr-fonts-style display-77">
-                                    <p>Psychotherapy</p>
-                                </div>
-                                <div className="progress_value mbr-fonts-style display-77">
-                                    <div className="progressbar-number"></div>
-                                    <span>%</span>
-                                </div>
-                            </div>
-                            <div className="progress">
-                                <div className="progress-bar progress-bar-2" role="progressbar" aria-valuenow="100"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-
-                        <div className="progress3 pb-3">
-                            <div className="title-wrap">
-                                <div className="progressbar-title mbr-fonts-style display-77">
-                                    <p>Clarity</p>
-                                </div>
-                                <div className="progress_value mbr-fonts-style display-77">
-                                    <div className="progressbar-number"></div>
-                                    <span>%</span>
-                                </div>
-                            </div>
-                            <div className="progress">
-                                <div className="progress-bar progress-bar-3" role="progressbar" aria-valuenow="100"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
