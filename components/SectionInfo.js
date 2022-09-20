5a5946eb2ce6b4ac0cf9fd3e8669d222a1afad0f
AgustinTafura <agustintafura@hotmail.com>
2022-09-20 15:59:30 -0300

    section titles updated

--- components/SectionInfo.js
+++ components/SectionInfo.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import { useRouter } from "next/router"
 
 const SectionInfo = () => {
+    const router = useRouter()
+    const {asPath} = router
+    const title = asPath.replace("/", '').replace("/", ' / ').replace("-", " ")
+
   return (
         <section className="info02 cid-rSVC8dfBcs" id="info2-1s">
         <div className="container align-center">
             <div className="row justify-content-center">
                 <div className="mbr-black col-md-12 col-lg-10">
-                    <h3 className="mbr-section-title mbr-bold mbr-white pb-2 mbr-fonts-style display-22">Content / Accordions / Shop</h3>
+                    <h3 className="mbr-section-title mbr-bold mbr-white pb-2 mbr-fonts-style display-22 text-capitalize">{title}</h3>
                 </div>
             </div>
         </div>
