b8a0bf5182400887f575b470a9ace3a4486efddf
AgustinTafura <agustintafura@hotmail.com>
2022-12-07 19:17:28 -0300

    sectionInfo header updated

--- components/SectionInfo.js
+++ components/SectionInfo.js
@@ -3,14 +3,18 @@ import { useRouter } from "next/router"
 const SectionInfo = () => {
     const router = useRouter()
     const {asPath} = router
-    const title = asPath.replace("/", '').replace("/", ' / ').replace("-", " ")
-
+    const title = asPath.replace("/", '').replace("/", ' /').replace("-", " ").replace('servicios /', '')
+    
   return (
         <section className="info02 cid-rSVC8dfBcs pb-3" id="info2-1s">
         <div className="container align-center">
             <div className="row justify-content-center">
                 <div className="mbr-black col-md-12 col-lg-10">
-                    <h3 className="mbr-section-title mbr-bold mbr-white pb-2 mbr-fonts-style display-22 text-capitalize">{title}</h3>
+                    <h3 className="mbr-section-title mbr-bold mbr-white pb-2 mbr-fonts-style display-22 text-capitalize">
+                        {
+                        title.includes('/') ? title.replace(title.substring(title.lastIndexOf('/'), title.length), '')  :title
+                        }
+                    </h3>
                 </div>
             </div>
         </div>
